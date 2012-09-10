116d00fcca6a5ee830e327bfb9cb0a8349fb6150
Dave Arneson <urvaius@gmail.com>
2012-09-10 16:42:58 -0700

    setting game screen

--- EzraTicTacToe/gen/com/arne5/ezratictactoe/R.java
+++ EzraTicTacToe/gen/com/arne5/ezratictactoe/R.java
@@ -13,12 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int vazio=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int txtplayerone=0x7f070000;
+        public static final int txtplayertwo=0x7f070001;
     }
     public static final class layout {
         public static final int activity_game_start=0x7f030000;
+        public static final int game=0x7f030001;
     }
     public static final class menu {
         public static final int activity_game_start=0x7f060000;
